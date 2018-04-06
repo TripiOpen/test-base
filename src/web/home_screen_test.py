de6fbf4c82443052519db7f61ef92d4789e46ab6
Deverlex <nguyendo94vn@gmail.com>
2018-04-06 23:55:00 +0700

    modify test 2 demo

--- src/web/home_screen_test.py
+++ src/web/home_screen_test.py
@@ -16,10 +16,9 @@ class HomeScreenTest(TestBase):
         self.assertEqual(self.driver.title.encode('utf-8'), "Tripi.vn - Đặt vé máy bay và khách sạn thuận tiện với giá tốt nhất", "title failed")
 
     def test2_find_ticket(self):
-        self.load_page("https://vnexpress.net/")
-        # self.driver.fullscreen_window()
-        submit_button = self.driver.find_elements_by_xpath("[//input[@name='btnK']")[0]
-        
+        self.load_page("https://www.tripi.vn/")
+        self.button = self.driver.find_elements_by_xpath('//*[@id="homepage-tripi-search-form"]/form/div[6]/button')[0]
+        self.button.click()
         sleep(2)
 
 if __name__ == "__main__":
