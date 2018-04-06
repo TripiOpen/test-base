2e9844b91ace2ffe52420f73fb8519e47e5a26e7
Spider man <nguyendo94vn@gmail.com>
2018-04-06 15:52:00 +0700

    update test

--- src/web/home_screen_test.py
+++ src/web/home_screen_test.py
@@ -4,17 +4,23 @@ import sys
 sys.path.append("/home/lion/tripi/test-base")
 
 import unittest
+
+from time import sleep
+
 from src.utils.test_base import TestBase
 
 class HomeScreenTest(TestBase):
 
-    def test_load_title(self):
+    def test1_load_title(self):
         self.load_page("https://www.tripi.vn/")
         self.assertEqual(self.driver.title.encode('utf-8'), "Tripi.vn - Đặt vé máy bay và khách sạn thuận tiện với giá tốt nhất", "title failed")
 
-    def test_find_ticket(self):
-        self.load_page("https://www.tripi.vn/")
-        self.driver.find_element_by_class_name("sfm-item flights").click()
+    def test2_find_ticket(self):
+        self.load_page("https://vnexpress.net/")
+        # self.driver.fullscreen_window()
+        submit_button = self.driver.find_elements_by_xpath("[//input[@name='btnK']")[0]
+        
+        sleep(2)
 
 if __name__ == "__main__":
     SUITE = unittest.TestLoader().loadTestsFromTestCase(HomeScreenTest)
